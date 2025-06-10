8512c035bdd1965be21a8dd9818c87492679da16
KK <k-kitamura@progresstech.jp>
2025-06-10 14:57:10 +0900

    CPU作成中

---
+++ ConsoleApplication2/utility.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct Point {
+    int x;    // X???W
+    int y;    // Y???W
+} Point;
+
+void printLocation(Point*);
+void move(Point*, int, int);
+
+#endif 
